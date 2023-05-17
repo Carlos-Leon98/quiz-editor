f055775435a268f37d60c8247d0163feecbb08dd
Carlos Leon <carlosaleonrivas@gmail.com>
2023-05-16 22:11:00 -0500

    Adding use effects to render the quizzes names

--- src/components/AddQuizForm.js
+++ src/components/AddQuizForm.js
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import { Plus } from 'react-bootstrap-icons';
+import React, { useState, useEffect } from "react";
+
 
 function AddQuizForm() {
   const [quizName, setQuizName] = useState("");
   const [question, setQuestion] = useState("");
+  const [quizList, setQuizList] = useState([]);
+
+  useEffect(() => {
+    const storedQuizList = JSON.parse(localStorage.getItem("quizList")) || [];
+    setQuizList(storedQuizList);
+  }, []);
 
   const handleQuizNameChange = (event) => {
     setQuizName(event.target.value);
@@ -17,14 +27,26 @@ function AddQuizForm() {
 
     const quiz = {
       name: quizName,
-      question: question,
+      questions: question,
       display: false
     };
 
-    const quizList = JSON.parse(localStorage.getItem("quizList")) || [];
-    quizList.push(quiz);
+    const updatedQuizList = [...quizList, quiz];
+    localStorage.setItem("quizList", JSON.stringify(updatedQuizList));
+    setQuizList(updatedQuizList);
 
-    localStorage.setItem("quizList", JSON.stringify(quizList));
+    setQuizName("");
+    setQuestion("");
+  };
+
+  const handleInsertQuestion = () => {
+    const updatedQuizList = [...quizList];
+    updatedQuizList.push({
+      name: quizName,
+      questions: question,
+      display: false
+    });
+    setQuizList(updatedQuizList);
 
     setQuizName("");
     setQuestion("");
@@ -52,6 +74,16 @@ function AddQuizForm() {
             value={question}
             onChange={handleQuestionChange}
           />
+        </div>
+        <div className='d-flex justify-content-end'>
+          <Button 
+            type='button'
+            className='mb-4'
+            variant='outline-success'
+            onClick={handleInsertQuestion}
+          >
+            <Plus /> Insert Another Question
+          </Button>
         </div>
         <button
           type="submit"
--- src/components/QuizTable.js
+++ src/components/QuizTable.js
@@ -1,45 +1,47 @@
+import React, { useState, useEffect } from "react";
 import DeleteButton from "./DeleteButton";
 
 function QuizTable() {
-    const quizList = JSON.parse(localStorage.getItem("quizList")) || [];
+  const [quizList, setQuizList] = useState([]);
 
-    const removeQuiz = (i) => {
-        const newQuizzes = [...quizList];
-        newQuizzes.splice(i, 1);
+  useEffect(() => {
+    const storedQuizList = JSON.parse(localStorage.getItem("quizList")) || [];
+    setQuizList(storedQuizList);
+  }, [quizList]);
 
-        localStorage.setItem("quizList", JSON.stringify(newQuizzes));
-    }
+  const removeQuiz = (i) => {
+    const newQuizzes = [...quizList];
+    newQuizzes.splice(i, 1);
+    setQuizList(newQuizzes);
+    localStorage.setItem("quizList", JSON.stringify(newQuizzes));
+  };
 
-    return (
-        <aside className="col-3">
-            <table className="table table-hover">
-                <thead className="thead-light">
-                    <tr>
-                        <th scope="col">
-                            Your Quizzes
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {quizList.map((quiz, index) => (
-                        <tr 
-                            key={index}
-                            onClick={() => {
-                                quiz.display = true;
-                            }}
-                        >
-                            <td className="d-flex justify-content-between">
-                                {quiz.name}
-                                <DeleteButton 
-                                    index={index}
-                                    onClick={() => removeQuiz(index)}
-                                />
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </aside>
-    );
+  return (
+    <aside className="col-3">
+      <table className="table table-hover">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col">Your Quizzes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {quizList.map((quiz, index) => (
+            <tr
+              key={index}
+              onClick={() => {
+                quiz.display = true;
+              }}
+            >
+              <td className="d-flex justify-content-between">
+                {quiz.name}
+                <DeleteButton index={index} onClick={() => removeQuiz(index)} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </aside>
+  );
 }
+
 export default QuizTable;
